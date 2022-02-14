9164d4e2f816151c6d70dbfa9d55a8ab2590f4da
Lucas Botingnon <lucas.botingnon@gmail.com>
2022-02-13 21:23:15 -0300

    no animation when resizing screen

--- components/header.js
+++ components/header.js
@@ -48,3 +48,12 @@ navToggle.addEventListener('click', () => {
 
 	navBar.setAttribute('data-visible', visibility === 'false' ? true : false)
 })
+
+let resizeTimer
+window.addEventListener('resize', () => {
+	document.body.classList.add('resize-animation-stopper')
+	clearTimeout(resizeTimer)
+	resizeTimer = setTimeout(() => {
+		document.body.classList.remove('resize-animation-stopper')
+	}, 400)
+})
